74c649b0f1f16e659a1a316893ae6395429fdb7c
Alexandre Rocca <alexandre.rocca@imag.fr>
2016-10-20 09:51:24 +0200

    update bench

--- FPBern(b)/do_all.m
+++ FPBern(b)/do_all.m
@@ -2,8 +2,8 @@ addpath(genpath('benchmarks/'))
 addpath(genpath('include/'))
 addpath(genpath('src/'))
 
-programs_names = {'simple_test';'rigidBody1';'rigidBody2';'kepler0real';'kepler1';'kepler2';'sineTaylor';'sineOrder3';'sqroot';'himmilbeau'};
-files_names = {'simple_test';'rigibody1real';'rigibody2real';'kepler0real';'kepler1real';'kepler2real';'sineTaylorreal';'sineOrder3real';'sqrootreal';'himmilbeaureal'};
+programs_names = {'simple_test';'rigidBody1';'rigidBody2';'kepler0real';'kepler1';'kepler2';'sineTaylor';'sineOrder3';'sqroot';'himmilbeau';'schwefel';'magnetism';'caprasse'};
+files_names = {'simple_test';'rigibody1real';'rigibody2real';'kepler0real';'kepler1real';'kepler2real';'sineTaylorreal';'sineOrder3real';'sqrootreal';'himmilbeaureal';'schwefel';'magnetism';'caprasse'};
 
 result = cell(3 ,size(files_names,1)+1);
 result{1,1} = 'Name  ';
